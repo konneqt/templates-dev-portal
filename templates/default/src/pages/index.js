3edbdcb1bb963f5e23d49c0f37337c6cc4ac354d
RaianeQriarLabs <raiane.coimbra@qriarlabs.com>
2025-04-03 15:46:25 -0300

    update redirect pages

--- templates/default/src/pages/index.js
+++ templates/default/src/pages/index.js
@@ -2,5 +2,5 @@ import React from 'react';
 import  { Redirect } from 'react-router-dom';
 
 export default function Home() {
-  return <Redirect to='/quantum-dev-portal/intro' />;
+  return <Redirect to='/quantum-dev-portal/docs' />;
 }
